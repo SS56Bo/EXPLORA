547ad0b1980ff90426e772b2472a1838b34daf32
SS56Bo <sohamsarkar560@gmail.com>
2024-05-26 23:29:29 +0530

    Added Tour Model and updated Schema

---
+++ models/tourModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const tourModel = new mongoose.schema({
+  tourName: {
+    type: String,
+    required: [true, 'A Tour must have a name'],
+    unique: true,
+    trim: true,
+  },
+  startingDate: {
+    type: Date,
+    require: [true, 'A tour must always have a starting data'],
+    unique: true,
+    trim: true,
+  },
+  numberOfDays: {
+    type: String,
+    require: [true, 'A tour must always have defined number of dates'],
+  },
+  priceOfTour: {
+    type: Number,
+    require: [true],
+    trim: true,
+  },
+  rating: {
+    type: Number,
+    trim: true,
+  },
+  discountPrice: {
+    type: Number,
+    trim: true,
+  },
+  descriptionTour: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
+});
+
+const tourInfo = mongoose.model('tourInfo', tourModel);
